895010efa6460cc5437bf63997c315f76de57e62
Amal Jose <amaljoseami@gmail.com>
2024-04-24 16:05:09 +0530

    Wip

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
-type Data = {
-  name: string;
-};
+import requestIp from "request-ip";
+import { headers } from "next/headers";
+type Data = any
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+
+    const detectedIp = requestIp.getClientIp(req);
+    const headersRaw = req.rawHeaders
+
+    console.log(req)
+
+  res.status(200).json({ name: "John Doe", detectedIp, headersRaw });
 }
